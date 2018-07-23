fd1d0ede8df1b813398619d00437a76e52ff74bd
dossa1020 <dossa0328@korea.ac.kr>
2018-07-23 16:58:12 +0900

    ts1

--- BNL.py
+++ BNL.py
@@ -1,1 +1,6 @@
-print('hello world')
+# data set 만들기 [가격 , 해변으로부터의 거리]
+data_set = [[10, 40], [45, 90], [35, 92], [43, 36], [49, 8], [4, 61], [11, 22], [48, 51], [92, 5]]
+# 크기가 3인 window set 만들기
+window_set = [i for i in range(3)]
+# 크기가 ? 인 temp set 만들기
+temp_set = []
