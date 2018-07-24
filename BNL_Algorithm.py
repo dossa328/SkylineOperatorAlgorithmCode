13c7ac8c841f79d5da37483946ba31988c60a810
dossa1020 <dossa0328@korea.ac.kr>
2018-07-24 11:57:24 +0900

    Basic Skyline + BNL 알고리즘

---
+++ BNL_Algorithm.py
@@ -0,0 +1,41 @@
+# data set 만들기 [가격 , 해변으로부터의 거리]
+data_set = [[45, 90], [35, 92], [43, 90], [49, 8], [4, 61], [10, 22], [48, 51], [92, 5], [10, 40]]
+# 크기가 3인 window set 만들기
+data1 = [i for i in range(1)]
+# 크기가 ? 인 temp set 만들기
+first_data = [0]
+second_data = [0]
+temp = [0]
+ck = 0
+dom = []
+skyline = []
+
+
+def dominated(looser, winner):
+    dim = len(looser)
+
+    # as good or better in all dimensions
+    for i in range(dim):
+        if winner[i] > looser[i]:
+            return False
+
+    # better in at least one dimension
+    for i in range(dim):
+        if winner[i] < looser[i]:
+            return True
+
+    return False
+
+
+for first_data in data_set:
+    for second_data in data_set:
+        if not dominated(first_data, second_data):
+            if skyline.__contains__(first_data):
+                pass
+            else:
+                skyline.append(first_data)
+        else:
+            if skyline.__contains__(first_data):
+                skyline.remove(first_data)
+                break
+print(skyline)
